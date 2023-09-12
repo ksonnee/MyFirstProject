68ad5ad010bfb5b97258a1beb9e2fcfe442eaf03
ksonnee <144365330+ksonnee@users.noreply.github.com>
2023-09-12 10:24:14 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+Kole Sonnenburg
+Fall 2023
+Due: Sept. 15
+Lab 1 "Hello World"
+Create a main function and output "Hello World!" 
+to the screen with your favorite color
+
+*/  
+
+#include <iostream>
+int main()
+{
+	system("Color 6");
+	std::cout << "Hello World!";
+	return 0;
+}
